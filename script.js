ac3ffea23a0ad0230b002dfe5d69dbeb8b97d54e
Matěj Brachtl <bra42176@spseol.cz>
2024-03-06 18:58:20 +0100

    JS - search-input

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-
-
-// document.querySelector('.navbar-search-button').addEventListener('click',()=>{
-//     const search = document.querySelector('.navbar-input')
-//     window.location.href = 'index.html#'+search.value
-//     console.log(window.location.href)
-// });
+document.querySelector('.navbar-search-button').addEventListener('click',()=>{
+    const search = document.querySelector('.navbar-input');
+    window.location.href = 'rozbory.html#'+search.value;
+    location.reload();
+});
+const searchText = decodeURIComponent(window.location.hash.substring(1));
+console.log("Vyhledávaný text: " + searchText);
