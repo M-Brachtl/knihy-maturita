9d728fd40dd9032c1a869b46d5ee13f33b7708f2
Matěj Brachtl <bra42176@spseol.cz>
2024-03-06 12:32:51 +0100

    eh

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// document.querySelector('.navbar-search-button').addEventListener('click',()=>{
+//     const search = document.querySelector('.navbar-input')
+//     window.location.href = 'index.html#'+search.value
+//     console.log(window.location.href)
+// });
