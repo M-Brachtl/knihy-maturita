ed8ad0fb617c9f79816c9479a65b787b2ee3fb4c
Matěj Brachtl <bra42176@spseol.cz>
2024-03-07 12:02:23 +0100

    JS 0.2

--- script.js
+++ script.js
@@ -1,37 +1,53 @@
-document.querySelectorAll('.navbar-search-button').forEach((element)=>{
-    element.addEventListener('click',()=>{
+document.querySelectorAll('.navbar-search-button').forEach((element) => {
+    element.addEventListener('click', () => {
         let search = document.querySelector('.navbar-input');
         if (search.value === "") {
             search = document.querySelector('.big input');
         };
-        window.location.href = 'rozbory.html#'+search.value;
+        window.location.href = 'rozbory.html#' + search.value;
         try {
-            
+
             const searchText = decodeURIComponent(window.location.hash.substring(1));
             console.log("Vyhledávaný text: " + searchText);
             document.querySelector('.big input').value = searchText;
             location.reload();
-        } catch (error) {};
+        } catch (error) { };
     });
 });
-document.addEventListener('keypress',(e)=>{
+let toSearch = "";
+document.addEventListener('keydown', (e) => {
+    try {
+        if ("abcdefghijklmnopqrstuvwxyzěščřžýáíéďťňóúů ".includes(e.key) || "ABCDEFGHIJKLMNOPQRSTUVWXYZĚŠČŘŽÝÁÍÉĎŤŇÓÚŮ ".includes(e.key)) {
+            toSearch += e.key;
+        } else if (e.key === "Backspace") {
+            toSearch = toSearch.substring(0, toSearch.length - 1);
+        };
+        searchBooks(toSearch);
+        
+    } catch (e) { };
     if (e.key === 'Enter') {
         let search = document.querySelector('.navbar-input');
         if (search.value === "") {
             search = document.querySelector('.big input');
         };
-        window.location.href = 'rozbory.html#'+search.value;
+        window.location.href = 'rozbory.html#' + search.value;
         try {
             const searchText = decodeURIComponent(window.location.hash.substring(1));
             console.log("Vyhledávaný text: " + searchText);
             document.querySelector('.big input').value = searchText;
             location.reload();
-        } catch (error) {};
+        } catch (error) { };
     };
 });
+
+function searchBooks(searchText) {
+    document.getElementById('test-search').innerHTML = searchText;
+};
+
+
 try {
     const searchText = decodeURIComponent(window.location.hash.substring(1));
     console.log("Vyhledávaný text: " + searchText);
     document.querySelector('.big input').value = searchText;
-    
-} catch (error) {};
+    searchBooks(searchText);
+} catch (error) { };
