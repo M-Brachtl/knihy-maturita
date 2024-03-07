84be3c6a7fdfa32221d9ca711d7dbd4e66687c8d
Matěj Brachtl <bra42176@spseol.cz>
2024-03-07 11:33:47 +0100

    JS - Enter

--- script.js
+++ script.js
@@ -13,11 +13,25 @@ document.querySelectorAll('.navbar-search-button').forEach((element)=>{
             location.reload();
         } catch (error) {};
     });
-    try {
-        const searchText = decodeURIComponent(window.location.hash.substring(1));
-        console.log("Vyhledávaný text: " + searchText);
-        document.querySelector('.big input').value = searchText;
-        
-    } catch (error) {};
-    
 });
+document.addEventListener('keypress',(e)=>{
+    if (e.key === 'Enter') {
+        let search = document.querySelector('.navbar-input');
+        if (search.value === "") {
+            search = document.querySelector('.big input');
+        };
+        window.location.href = 'rozbory.html#'+search.value;
+        try {
+            const searchText = decodeURIComponent(window.location.hash.substring(1));
+            console.log("Vyhledávaný text: " + searchText);
+            document.querySelector('.big input').value = searchText;
+            location.reload();
+        } catch (error) {};
+    };
+});
+try {
+    const searchText = decodeURIComponent(window.location.hash.substring(1));
+    console.log("Vyhledávaný text: " + searchText);
+    document.querySelector('.big input').value = searchText;
+    
+} catch (error) {};
