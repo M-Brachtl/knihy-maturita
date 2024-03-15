651e98c3173f8be72cef027e2a12332362aa3953
Matěj Brachtl <bra42176@spseol.cz>
2024-03-15 15:07:29 +0100

    Mobilní JS atd.

--- script.js
+++ script.js
@@ -14,23 +14,33 @@ document.querySelectorAll('.navbar-search-button').forEach((element) => {
         } catch (error) { };
     });
 });
+// function sectionPDF(bookInfo){
+//     console.log("AAAAAAAAAAAAAAAAAAAAAAAAa");
+//     document.querySelector('object').data='${book.path}';
+//     // mobile view
+//     const mobileView = document.querySelector('#mobile-pdf');
+//     mobileView.childNodes[0].textContent = bookInfo.name;
+//     mobileView.childNodes[1].textContent = bookInfo.author;
+//     mobileView.childNodes[2].href = bookInfo.path;
+// }
 let toSearch = "";
 document.addEventListener('keydown', (e) => {
     setTimeout(() => {
-    if(document.activeElement === document.querySelector('.big input')){
-    try {
-        // if (("abcdefghijklmnopqrstuvwxyzěščřžýáíéďťňóúů ".includes(e.key) || "ABCDEFGHIJKLMNOPQRSTUVWXYZĚŠČŘŽÝÁÍÉĎŤŇÓÚŮ ".includes(e.key)) && e.ctrlKey === false) {
-        //     toSearch =document.querySelector('.big input').value;
-        // } else if (e.key === "Backspace") {
-        //     toSearch = document.querySelector('.big input').value//toSearch.substring(0, toSearch.length - 1);
-        // } else {
-        //     toSearch = document.querySelector('.big input').value;
-        // }
-        toSearch =document.querySelector('.big input').value;
-        searchBooks(toSearch);
-    } catch (e) { };
-    console.log(toSearch);
-    }},1);
+        if (document.activeElement === document.querySelector('.big input')) {
+            try {
+                // if (("abcdefghijklmnopqrstuvwxyzěščřžýáíéďťňóúů ".includes(e.key) || "ABCDEFGHIJKLMNOPQRSTUVWXYZĚŠČŘŽÝÁÍÉĎŤŇÓÚŮ ".includes(e.key)) && e.ctrlKey === false) {
+                //     toSearch =document.querySelector('.big input').value;
+                // } else if (e.key === "Backspace") {
+                //     toSearch = document.querySelector('.big input').value//toSearch.substring(0, toSearch.length - 1);
+                // } else {
+                //     toSearch = document.querySelector('.big input').value;
+                // }
+                toSearch = document.querySelector('.big input').value;
+                searchBooks(toSearch);
+            } catch (e) { };
+            console.log(toSearch);
+        }
+    }, 1);
     if (e.key === 'Enter') {
         let search = document.querySelector('.navbar-input');
         if (search.value === "") {
@@ -48,8 +58,8 @@ document.addEventListener('keydown', (e) => {
 
 function searchBooks(searchText) {
     const books = document.querySelectorAll('.seznam-knih li');
-    books.forEach((book)=>{
-        if(book.textContent.toLowerCase().includes(searchText.toLowerCase())){
+    books.forEach((book) => {
+        if (book.textContent.toLowerCase().includes(searchText.toLowerCase())) {
             book.style.display = "list-item";
         } else {
             book.style.display = "none";
@@ -68,11 +78,23 @@ async function getBooks() {
     const response = await fetch('knihy.json');
     const data = await response.json();
     data.forEach((book) => {
-        document.querySelector('.seznam-knih').innerHTML += `<li><a class="seznam-button" onclick="document.querySelector('object').data='${book.path}'">${book.name}</a><span>- ${book.author}</span></li>`;
+        document.querySelector('.seznam-knih').innerHTML += `<li><a class="seznam-button">${book.name}</a><span>- ${book.author}</span></li>`;
     });
+    for (let i = 0; i < data.length; i++) {
+        let book = data[i];
+        document.querySelector('.seznam-knih').childNodes[i+1].childNodes[0].addEventListener('click',() => {
+            console.log("AAAAAAAAAAAAAAAAAAAAAAAAa");
+            document.querySelector('object').data = book.path;
+            // mobile view
+            const mobileView = document.querySelector('#mobile-pdf');
+            mobileView.childNodes[1].textContent = book.name;
+            mobileView.childNodes[3].textContent = book.author;
+            mobileView.childNodes[5].href = book.path;
+        });
+    };
     document.querySelectorAll('li').forEach((button) => {
-        if(button.childNodes[0].getBoundingClientRect().width < button.childNodes[1].clientWidth){
-            button.childNodes[0].style.paddingRight =  button.childNodes[1].clientWidth - button.childNodes[0].getBoundingClientRect().width + 16 + 4 + "px";
+        if (button.childNodes[0].getBoundingClientRect().width < button.childNodes[1].clientWidth) {
+            button.childNodes[0].style.paddingRight = button.childNodes[1].clientWidth - button.childNodes[0].getBoundingClientRect().width + 16 + 4 + "px";
         }
     });
 };
@@ -82,21 +104,20 @@ try {
         console.log("Vyhledávaný text: " + searchText);
         document.querySelector('.big input').value = searchText;
         searchBooks(searchText);
-        // if(document.querySelector('.navbar-heading').style.visibility === "hidden"){
-        //     document.querySelector('object').hidden = true;
-        //     document.querySelector('.mobile-pdf').hidden = false;
-        // }
-        // document.querySelector('.navbar-heading').visi
+        if (document.querySelector('.navbar-heading').clientWidth == 0) {
+            console.log("Mobilní zařízení");
+            document.querySelector('object').hidden = true;
+            document.getElementById('mobile-pdf').hidden = false;
+        }
     });
-} catch (error) {};
-
+} catch (error) { };
 
 
 // Adamův prostor //
 
 const navButtonMenu = document.querySelector(".menu-button");
 const dropMenu = document.querySelector(".dropdown");
-
+dropMenu.style.display = "none";
 navButtonMenu.addEventListener("click", () => {
     if (dropMenu.style.display === "none") {
         dropMenu.style.display = "block";
